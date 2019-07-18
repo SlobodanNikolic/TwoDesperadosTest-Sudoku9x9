e856d77e1d5e7123aa0959de14d08d3264e8b966
BigMac <cobibitza@gmail.com>
2019-07-18 17:44:39 +0200

    Added the dial in the scene

---
+++ TwoDesperadosSudoku/Assets/Scripts/ClickControler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonClicked(){
+
+        GameObject selectedObject = EventSystem.current.currentSelectedGameObject;
+        if (selectedObject != null)
+        {
+         //Selektovali smo dugme
+            Debug.Log("Clicked on : " + selectedObject.name);
+        }
+
+    }
+}
