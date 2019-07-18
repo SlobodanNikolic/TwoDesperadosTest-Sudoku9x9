6ef492be96bfbee33810810d9937eb6edbbeb0de
BigMac <cobibitza@gmail.com>
2019-07-18 21:50:07 +0200

    Working on Home screen functionality

--- TwoDesperadosSudoku/Assets/Scripts/Game.cs
+++ TwoDesperadosSudoku/Assets/Scripts/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
@@ -11,7 +12,39 @@ public class Game : MonoBehaviour
         GAME_OVER = 3
     }
 
+    public enum GAME_MODE{
+        EASY = 0,
+        HARD = 1
+    }
+
+    [SerializeField]
     private GAME_STATE currentState = GAME_STATE.HOME;
+    [SerializeField]
+    private GAME_MODE currentDifficulty = GAME_MODE.EASY;
+    [SerializeField]
+    private Sprite doorEasyOpen;
+    [SerializeField]
+    private Sprite doorEasyClosed;
+    [SerializeField]
+    private Sprite doorHardOpen;
+    [SerializeField]
+    private Sprite doorHardClosed;
+    [SerializeField]
+    private Button leverButton;
+    [SerializeField]
+    private Button doorButtonEasy;
+    [SerializeField]
+    private Button doorButtonHard;
+
+    [SerializeField]
+    private GameObject homePanel;
+
+    [SerializeField]
+    private GameObject gamePanel;
+
+    [SerializeField]
+    private GameObject gameOverPanel;
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,17 +58,53 @@ public class Game : MonoBehaviour
         
     }
 
+    public void ChangeDifficulty(){
+        if(currentDifficulty == GAME_MODE.EASY){
+
+            currentDifficulty = GAME_MODE.HARD;
+            leverButton.GetComponent<RectTransform>().RotateAround(Vector3.up, 180f);
+            doorButtonHard.gameObject.SetActive(true);
+            doorButtonEasy.gameObject.SetActive(false);
+
+        }
+        else
+        {
+            currentDifficulty = GAME_MODE.EASY;
+            leverButton.GetComponent<RectTransform>().RotateAround(Vector3.up, 180f);
+            doorButtonHard.gameObject.SetActive(false);
+            doorButtonEasy.gameObject.SetActive(true);
+
+        }
+    }
+
+    public void Play(){
+        Invoke("Ready", 2f);
+    }
+
     public void SetGameState(GAME_STATE newState){
         switch(newState){
 
             case GAME_STATE.HOME:
+                homePanel.SetActive(true);
+                gameOverPanel.SetActive(false);
+                gamePanel.SetActive(false);
                 break;
             case GAME_STATE.READY:
+                homePanel.SetActive(false);
+                gameOverPanel.SetActive(false);
+                gamePanel.SetActive(false);
                 break;
             case GAME_STATE.PLAYING:
                 break;
             case GAME_STATE.GAME_OVER:
+                homePanel.SetActive(false);
+                gameOverPanel.SetActive(true);
+                gamePanel.SetActive(false);
                 break;
         }
+    }
+
+    public void Ready(){
+        SetGameState(GAME_STATE.READY);
     }
 }
